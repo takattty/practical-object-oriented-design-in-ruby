c732d0bf093dbc30a5b54875d42885a8fccbcfea
takattty <r9r_celtics_t9t@icloud.com>
2021-07-22 12:12:45 +0900

    first commit

---
+++ chapter2/app.rb
@@ -0,0 +1,34 @@
+class Gear
+  # インスタンス変数を呼び出す為だけのメソッド
+  # 各メソッドが振る舞い（1箇所で定義される）になっている。
+  attr_reader :chainring, :cog, :rim, :tire
+
+  # def chainring
+  #   @chainring
+  # end
+
+  # def cog
+  #   @cog
+  # end
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog       = cog
+    @rim       = rim
+    @tire      = tire
+  end
+
+  def ratio
+    # ここは変数を呼び出しているのではなく、上のアクセサメソッドを呼び出している
+    # 変数をクラスからも隠蔽し、直接参照されない様にしている。
+    # to_f = to floatのメソッド
+    chainring / cog.to_f 
+  end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(30, 27, 24, 1.25).gear_inches
---
+++ chapter2/obscuringReferences.rb
@@ -0,0 +1,29 @@
+class ObscuringReferences
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def diameters
+    data.collect{ |cell| cell[0] + (cell[1] * 2) }
+  end
+end
+
+class RevealingReferences
+  attr_reader :wheels
+
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect { |wheel| wheel.rim + (wheel.tire * 2) }
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+
+  def wheelify(data)
+    data.collect { |cell| Wheel.new(cell[0], cell[1]) }
+  end
+end
