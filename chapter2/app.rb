96d42ccccdf435d5ef36a54e7e6380370b028e36
takattty <r9r_celtics_t9t@icloud.com>
2021-07-23 13:41:28 +0900

    isolate wheel class and refactoring

--- chapter2/app.rb
+++ chapter2/app.rb
@@ -11,17 +11,17 @@ class Gear
   #   @cog
   # end
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog       = cog
-    @wheel     = Wheel.new(rim. tire)
+    @wheel     = wheel
   end
 
   def ratio
     # ここは変数を呼び出しているのではなく、上のアクセサメソッドを呼び出している
     # 変数をクラスからも隠蔽し、直接参照されない様にしている。
     # to_f = to floatのメソッド
-    chainring / cog.to_f 
+    chainring / cog.to_f
   end
 
   def gear_inches
@@ -30,13 +30,26 @@ class Gear
     ratio * wheel.diameter
   end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
-  end
-
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(30, 27, 24, 1.25).gear_inches
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim  = rim
+    @tire = tire
+  end
+
+  def diameter # 直径
+    rim + (tire * 2)
+  end
+
+  def circumference # 周
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+p @wheel.circumference
+p Gear.new(52, 11, @wheel).gear_inches
+p Gear.new(52, 11).ratio
